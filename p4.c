3ab14c114855282ca83a31e917c0ebb5f7f94a9e
Sonia <soniakumari00881@gmail.com>
2026-07-01 13:48:39 +0530

    Refactor temperature variable name to 'Fahrenheit'
    Updated variable name from 'F' to 'Fahrenheit' for clarity.

--- p4.c
+++ p4.c
@@ -4,12 +4,12 @@
 
 int main() {
 
-float  temp, F;
+float  temp, Fahrenheit;
  
   printf("Enter the temp in celsius:");
   scanf("%f",&temp);
 
-  F= 9/5*(temp) + 32.0;
+  Fahrenheit= 9/5*(temp) + 32.0;
 
     printf("The temp in fahrenheit is %.2f",F);
 
