b925e3737a84e2951932532417e6903ba6391c0b
Sonia <soniakumari00881@gmail.com>
2026-07-04 17:16:17 +0530

    Correct variable names and improve input prompts
    Fixed variable names and added missing variable declarations.

--- p10.c
+++ p10.c
@@ -9,7 +9,7 @@ int English,physics,chemistry,EE,hindi, sum,percentage;
     printf("Enter English no :");
     scanf("%d",&English);
 
-    printf("Enter phy no :");
+    printf("Enter physics no :");
     scanf("%d",&phy);
 
     printf("Enter chem no :");
