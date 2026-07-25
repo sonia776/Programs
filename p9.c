0b2f75722a53a27589ffba779ac0c353215c48db
Sonia <soniakumari00881@gmail.com>
2026-07-25 17:11:37 +0530

    Fix formatting and clean up leap year check code

--- p9.c
+++ p9.c
@@ -11,7 +11,7 @@ int main() {
      scanf("%d",&year);
 
 if(year % 4 == 0 && year % 100 !=0 || year % 400 == 0  ){
-  printf("%d is a leap year",year);
+  printf(" %d is a leap year",year);
 }
 
   else {
