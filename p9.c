46a1162cec29b85a46e28c81a31a7f3eb3b33a8b
Sonia <soniakumari00881@gmail.com>
2026-08-01 22:37:56 +0530

    Fix formatting and syntax in leap year checker

--- p9.c
+++ p9.c
@@ -8,7 +8,7 @@ int main() {
     int year;
 
     printf(" Enter the year: ");
-     scanf(" %d",&year);
+     scanf(" %d ",&year);
 
 if(year % 4 == 0 && year % 100 !=0 || year % 400 == 0  ){
   printf(" %d is a leap year",year);
